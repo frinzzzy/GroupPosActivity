0ee1e7ee1f01bb481b024afc3814af3cd0773e25
Asuf_Tuf Gaming <Asuf_Tuf Gaming@LAPTOP-ML4C5R4L>
2026-02-04 21:59:05 +0800

    Add project files.

---
+++ GroupPosActivity/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace GroupPosActivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            whopper = new PictureBox();
+            stacker = new PictureBox();
+            plantbased = new PictureBox();
+            rodeo = new PictureBox();
+            xtralong = new PictureBox();
+            bigking = new PictureBox();
+            chking = new PictureBox();
+            nuggets = new PictureBox();
+            fourcheese = new PictureBox();
+            baconking = new PictureBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)whopper).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)stacker).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)plantbased).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)rodeo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)xtralong).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bigking).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chking).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nuggets).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)fourcheese).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)baconking).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 62);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Silver;
+            pictureBox2.Image = Properties.Resources.burger_king_logo_xua_removebg_preview;
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(277, 236);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // whopper
+            // 
+            whopper.BackColor = Color.White;
+            whopper.Cursor = Cursors.Hand;
+            whopper.Image = (Image)resources.GetObject("whopper.Image");
+            whopper.Location = new Point(47, 610);
+            whopper.Name = "whopper";
+            whopper.Size = new Size(175, 164);
+            whopper.SizeMode = PictureBoxSizeMode.Zoom;
+            whopper.TabIndex = 3;
+            whopper.TabStop = false;
+            whopper.Click += pictureBox3_Click;
+            whopper.MouseEnter += whopper_MouseEnter;
+            whopper.MouseLeave += whopper_MouseLeave;
+            // 
+            // stacker
+            // 
+            stacker.BackColor = Color.White;
+            stacker.Cursor = Cursors.Hand;
+            stacker.Image = Properties.Resources.doublestacker;
+            stacker.Location = new Point(47, 848);
+            stacker.Name = "stacker";
+            stacker.Size = new Size(175, 164);
+            stacker.SizeMode = PictureBoxSizeMode.Zoom;
+            stacker.TabIndex = 4;
+            stacker.TabStop = false;
+            stacker.MouseEnter += stacker_MouseEnter;
+            stacker.MouseLeave += stacker_MouseLeave;
+            // 
+            // plantbased
+            // 
+            plantbased.BackColor = Color.White;
+            plantbased.Cursor = Cursors.Hand;
+            plantbased.Image = Properties.Resources.Plantbased;
+            plantbased.Location = new Point(320, 848);
+            plantbased.Name = "plantbased";
+            plantbased.Size = new Size(175, 164);
+            plantbased.SizeMode = PictureBoxSizeMode.StretchImage;
+            plantbased.TabIndex = 6;
+            plantbased.TabStop = false;
+            plantbased.MouseEnter += plantbased_MouseEnter;
+            plantbased.MouseLeave += plantbased_MouseLeave;
+            // 
+            // rodeo
+            // 
+            rodeo.BackColor = Color.White;
+            rodeo.Cursor = Cursors.Hand;
+            rodeo.Image = (Image)resources.GetObject("rodeo.Image");
+            rodeo.Location = new Point(606, 848);
+            rodeo.Name = "rodeo";
+            rodeo.Size = new Size(175, 164);
+            rodeo.SizeMode = PictureBoxSizeMode.Zoom;
+            rodeo.TabIndex = 8;
+            rodeo.TabStop = false;
+            rodeo.MouseEnter += rodeo_MouseEnter;
+            rodeo.MouseLeave += rodeo_MouseLeave;
+            // 
+            // xtralong
+            // 
+            xtralong.BackColor = Color.White;
+            xtralong.Cursor = Cursors.Hand;
+            xtralong.Image = Properties.Resources.extralong;
+            xtralong.Location = new Point(898, 848);
+            xtralong.Name = "xtralong";
+            xtralong.Size = new Size(175, 164);
+            xtralong.SizeMode = PictureBoxSizeMode.StretchImage;
+            xtralong.TabIndex = 10;
+            xtralong.TabStop = false;
+            xtralong.MouseEnter += xtralong_MouseEnter;
+            xtralong.MouseLeave += xtralong_MouseLeave;
+            // 
+            // bigking
+            // 
+            bigking.BackColor = Color.White;
+            bigking.Cursor = Cursors.Hand;
+            bigking.Image = Properties.Resources.bigxl;
+            bigking.Location = new Point(1160, 848);
+            bigking.Name = "bigking";
+            bigking.Size = new Size(175, 164);
+            bigking.SizeMode = PictureBoxSizeMode.StretchImage;
+            bigking.TabIndex = 12;
+            bigking.TabStop = false;
+            bigking.Click += pictureBox12_Click;
+            bigking.MouseEnter += bigking_MouseEnter;
+            bigking.MouseLeave += bigking_MouseLeave;
+            // 
+            // chking
+            // 
+            chking.BackColor = Color.White;
+            chking.Cursor = Cursors.Hand;
+            chking.Image = Properties.Resources.chikenking;
+            chking.Location = new Point(1160, 610);
+            chking.Name = "chking";
+            chking.Size = new Size(175, 164);
+            chking.SizeMode = PictureBoxSizeMode.Zoom;
+            chking.TabIndex = 11;
+            chking.TabStop = false;
+            chking.MouseEnter += chking_MouseEnter;
+            chking.MouseLeave += chking_MouseLeave;
+            // 
+            // nuggets
+            // 
+            nuggets.BackColor = Color.White;
+            nuggets.Cursor = Cursors.Hand;
+            nuggets.ErrorImage = (Image)resources.GetObject("nuggets.ErrorImage");
+            nuggets.Image = Properties.Resources.chicknugg;
+            nuggets.Location = new Point(898, 610);
+            nuggets.Name = "nuggets";
+            nuggets.Size = new Size(175, 164);
+            nuggets.SizeMode = PictureBoxSizeMode.Zoom;
+            nuggets.TabIndex = 9;
+            nuggets.TabStop = false;
+            nuggets.MouseEnter += nuggets_MouseEnter;
+            nuggets.MouseLeave += nuggets_MouseLeave;
+            // 
+            // fourcheese
+            // 
+            fourcheese.BackColor = Color.White;
+            fourcheese.Cursor = Cursors.Hand;
+            fourcheese.Image = Properties.Resources.fourcheesewhopper;
+            fourcheese.Location = new Point(606, 610);
+            fourcheese.Name = "fourcheese";
+            fourcheese.Size = new Size(175, 164);
+            fourcheese.SizeMode = PictureBoxSizeMode.StretchImage;
+            fourcheese.TabIndex = 7;
+            fourcheese.TabStop = false;
+            fourcheese.MouseEnter += fourcheese_MouseEnter;
+            fourcheese.MouseLeave += fourcheese_MouseLeave;
+            // 
+            // baconking
+            // 
+            baconking.BackColor = Color.White;
+            baconking.Cursor = Cursors.Hand;
+            baconking.Image = Properties.Resources.baconking;
+            baconking.Location = new Point(320, 610);
+            baconking.Name = "baconking";
+            baconking.Size = new Size(175, 164);
+            baconking.SizeMode = PictureBoxSizeMode.Zoom;
+            baconking.TabIndex = 5;
+            baconking.TabStop = false;
+            baconking.MouseEnter += baconking_MouseEnter;
+            baconking.MouseLeave += baconking_MouseLeave;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(295, 9);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1009, 227);
+            dataGridView1.TabIndex = 13;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            BackColor = Color.DimGray;
+            ClientSize = new Size(1448, 1055);
+            Controls.Add(dataGridView1);
+            Controls.Add(pictureBox2);
+            Controls.Add(bigking);
+            Controls.Add(chking);
+            Controls.Add(xtralong);
+            Controls.Add(nuggets);
+            Controls.Add(rodeo);
+            Controls.Add(fourcheese);
+            Controls.Add(plantbased);
+            Controls.Add(baconking);
+            Controls.Add(stacker);
+            Controls.Add(whopper);
+            Controls.Add(pictureBox1);
+            ForeColor = Color.Silver;
+            Name = "Form1";
+            RightToLeftLayout = true;
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)whopper).EndInit();
+            ((System.ComponentModel.ISupportInitialize)stacker).EndInit();
+            ((System.ComponentModel.ISupportInitialize)plantbased).EndInit();
+            ((System.ComponentModel.ISupportInitialize)rodeo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)xtralong).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bigking).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chking).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nuggets).EndInit();
+            ((System.ComponentModel.ISupportInitialize)fourcheese).EndInit();
+            ((System.ComponentModel.ISupportInitialize)baconking).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox whopper;
+        private PictureBox stacker;
+        private PictureBox plantbased;
+        private PictureBox rodeo;
+        private PictureBox xtralong;
+        private PictureBox bigking;
+        private PictureBox chking;
+        private PictureBox nuggets;
+        private PictureBox fourcheese;
+        private PictureBox baconking;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ GroupPosActivity/Form1.cs
@@ -0,0 +1,176 @@
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace GroupPosActivity
+{
+    // The Designer wants Form1 to be the FIRST class in the namespace
+    public partial class Form1 : Form
+    {
+        int burgerCount = 0;
+
+        // This list will hold your grid rows
+        BindingList<Product> cartItems = new BindingList<Product>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            whopper.Cursor = Cursors.Hand;
+
+            // Link your list to the data grid
+            dataGridView1.DataSource = cartItems;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Add a product to the grid when clicked
+            cartItems.Add(new Product { Name = "Whopper", Price = 150.00m });
+
+            burgerCount++;
+            MessageBox.Show("Done buying");
+        }
+
+        private void whopper_MouseEnter(object sender, EventArgs e)
+        {
+            whopper.Width += 10; whopper.Height += 10;
+            whopper.Left -= 5; whopper.Top -= 5;
+        }
+
+        private void whopper_MouseLeave(object sender, EventArgs e)
+        {
+            whopper.Width -= 10; whopper.Height -= 10;
+            whopper.Left += 5; whopper.Top += 5;
+        }
+
+        // --- Keep your other empty events below ---
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void pictureBox3_Click(object sender, EventArgs e) { }
+        private void pictureBox12_Click(object sender, EventArgs e) { }
+        private void whopperlabel_Click(object sender, EventArgs e) { }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void baconking_MouseEnter(object sender, EventArgs e)
+        {
+            baconking.Width += 10; baconking.Height += 10;
+            baconking.Left -= 5; baconking.Top -= 5;
+        }
+
+        private void baconking_MouseLeave(object sender, EventArgs e)
+        {
+            baconking.Width -= 10; baconking.Height -= 10;
+            baconking.Left += 5; baconking.Top += 5;
+        }
+
+        private void fourcheese_MouseEnter(object sender, EventArgs e)
+        {
+            fourcheese.Width += 10; fourcheese.Height += 10;
+            fourcheese.Left -= 5; fourcheese.Top -= 5;
+
+        }
+
+        private void fourcheese_MouseLeave(object sender, EventArgs e)
+        {
+            fourcheese.Width -= 10; fourcheese.Height -= 10;
+            fourcheese.Left += 5; fourcheese.Top += 5;
+        }
+
+        private void nuggets_MouseEnter(object sender, EventArgs e)
+        {
+            nuggets.Width += 10; nuggets.Height += 10;
+            nuggets.Left -= 5; nuggets.Top -= 5;
+        }
+
+        private void nuggets_MouseLeave(object sender, EventArgs e)
+        {
+
+            nuggets.Width -= 10; nuggets.Height -= 10;
+            nuggets.Left += 5; nuggets.Top += 5;
+        }
+
+        private void chking_MouseEnter(object sender, EventArgs e)
+        {
+            chking.Width += 10; chking.Height += 10;
+            chking.Left -= 5; chking.Top -= 5;
+        }
+
+        private void chking_MouseLeave(object sender, EventArgs e)
+        {
+            chking.Width -= 10; chking.Height -= 10;
+            chking.Left += 5; chking.Top += 5;
+        }
+
+        private void stacker_MouseEnter(object sender, EventArgs e)
+        {
+            stacker.Width += 10; stacker.Height += 10;
+            stacker.Left -= 5; stacker.Top -= 5;
+        }
+
+        private void stacker_MouseLeave(object sender, EventArgs e)
+        {
+            stacker.Width -= 10; stacker.Height -= 10;
+            stacker.Left += 5; stacker.Top += 5;
+        }
+
+        private void plantbased_MouseEnter(object sender, EventArgs e)
+        {
+            plantbased.Width += 10; plantbased.Height += 10;
+            plantbased.Left -= 5; plantbased.Top -= 5;
+        }
+
+        private void plantbased_MouseLeave(object sender, EventArgs e)
+        {
+            plantbased.Width -= 10; plantbased.Height -= 10;
+            plantbased.Left += 5; plantbased.Top += 5;
+        }
+
+        private void rodeo_MouseEnter(object sender, EventArgs e)
+        {
+            rodeo.Width += 10; rodeo.Height += 10;
+            rodeo.Left -= 5; rodeo.Top -= 5;
+        }
+
+        private void rodeo_MouseLeave(object sender, EventArgs e)
+        {
+            rodeo.Width -= 10; rodeo.Height -= 10;
+            rodeo.Left += 5; rodeo.Top += 5;
+        }
+
+        private void xtralong_MouseEnter(object sender, EventArgs e)
+        {
+            xtralong.Width += 10; xtralong.Height += 10;
+            xtralong.Left -= 5; xtralong.Top -= 5;
+        }
+
+        private void xtralong_MouseLeave(object sender, EventArgs e)
+        {
+
+            xtralong.Width -= 10; xtralong.Height -= 10;
+            xtralong.Left += 5; xtralong.Top += 5;
+        }
+
+        private void bigking_MouseEnter(object sender, EventArgs e)
+        {
+            bigking.Width += 10; bigking.Height += 10;
+            bigking.Left -= 5;  bigking.Top -= 5;
+        }
+
+        private void bigking_MouseLeave(object sender, EventArgs e)
+        {
+            bigking.Width -= 10; bigking.Height -= 10;
+            bigking.Left += 5; bigking.Top += 5;
+        }
+    }
+
+    // MOVE THE PRODUCT CLASS HERE (Bottom of the file)
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
